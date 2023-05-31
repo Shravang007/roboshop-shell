a48a7fc587db56e58b980eaa565a784843a03a40
Shravang007 <shravangoudgajula@gmail.com>
2023-05-31 20:54:02 +0530

    User Server Setup

---
+++ user.sh
@@ -0,0 +1,44 @@
+
+echo -e "\e[33mConfiguring NodeJS Repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+
+echo -e "\e[33mInstall NodeJs\e[0m"
+yum install nodejs -y
+
+echo -e "\e[33mAdd Application User\e[0m"
+useradd roboshop
+
+echo -e "\e[33mCreate Application Directory\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[33mDownloading Application Content\e[0m"
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+
+echo -e "\e[33mExtract Application Content\e[0m"
+unzip /tmp/user.zip
+cd /app
+
+echo -e "\e[33mInstall NodeJs Dependencies\e[0m"
+npm install
+
+echo -e "\e[33m\e[0m"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[33mStart User Service\e[0m"
+systemctl daemon-reload
+systemctl enable user
+systemctl restart user
+
+echo -e "\e[33mCopy Mongodb Repo File\e[0m"
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+echo -e "\e[33m\e[0m"
+yum install mongodb-org-shell -y
+
+echo -e "\e[33m\e[0m"
+mongo --host mongodb-dev.devopspractice73.online </app/schema/user.js
+
+
