6d6d5e8748ae6f2dcb1eedee1b3670a4c2d87fc6
Shravang007 <shravangoudgajula@gmail.com>
2023-05-31 19:41:40 +0530

    start nginx server

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ echo -e "\e[33mInstalling nginx server\e[0m"
 yum install nginx -y  &>>/tmp/roboshop.log
 
 echo -e "\e[33mDeleting Previous app content\e[0m"
-rm -rf /usr/share/nginx/html/* &> &>>/tmp/roboshop.log 
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33mDownloading frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 2>/tmp/error.log
