73487c542b36870ef6833c44d4e9094a039d5dac
Shravang007 <shravangoudgajula@gmail.com>
2023-05-31 13:19:39 +0530

    changing address of port

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ echo -e "\e[33mInstalling mongodb\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mUpdating Mongodb Listen address\e[0m"
-sed -i 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33mstarting mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
