69c32f8456970999b4b9f84895cacad80281848e
Shravang007 <shravangoudgajula@gmail.com>
2023-05-31 14:01:36 +0530

    Catalogue Server Setup

---
+++ catalogue.sh
@@ -0,0 +1,41 @@
+echo -e "\e[33mConfiguring NodeJs Repos\e[0m"
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[33mInstalling NodeJs\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m\add application user\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[33mCreate Application Directory\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[33mDownload application content\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+cd /app
+
+echo -e "\e[33mExtracting Application content\e[0m"
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+cd /app
+
+echo -e "\e[33mInstall NodeJs Dependencies\e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[33mSetup System Service\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+
+echo -e "\e[33mStart Catalogue Service\e[0m"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+
+echo -e "\e[33mCopy Mongodb Repo File\e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33mInstall Mongodb client\e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mLoad Schema\e[0m"
+mongo --host mongodb-dev.devopspractice73.online </app/schema/catalogue.js &>>/tmp/roboshop.log
