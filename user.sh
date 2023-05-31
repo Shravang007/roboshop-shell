83948706686a73f89d8734e235231afbab5e4d40
Shravang007 <shravangoudgajula@gmail.com>
2023-05-31 20:55:14 +0530

    User Server Setup

--- user.sh
+++ user.sh
@@ -1,44 +1,43 @@
 
 echo -e "\e[33mConfiguring NodeJS Repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
 
 echo -e "\e[33mInstall NodeJs\e[0m"
-yum install nodejs -y
+yum install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mAdd Application User\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[33mCreate Application Directory\e[0m"
-rm -rf /app
-mkdir /app
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[33mDownloading Application Content\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 echo -e "\e[33mExtract Application Content\e[0m"
-unzip /tmp/user.zip
-cd /app
+unzip /tmp/user.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall NodeJs Dependencies\e[0m"
-npm install
+npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33m\e[0m"
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
 
 echo -e "\e[33mStart User Service\e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable user &>>/tmp/roboshop.log
+systemctl restart user &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopy Mongodb Repo File\e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33m\e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m\e[0m"
-mongo --host mongodb-dev.devopspractice73.online </app/schema/user.js
+mongo --host mongodb-dev.devopspractice73.online </app/schema/user.js &>>/tmp/roboshop.log
 
 
