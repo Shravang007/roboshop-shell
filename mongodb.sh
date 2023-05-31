bdcae562cc3f6a48d9fabb2afe4327549586969d
Shravang007 <shravangoudgajula@gmail.com>
2023-05-31 13:10:55 +0530

    changing address of port

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,13 @@
 
-echo -e "\e[32mMongodb repo file\e[0m"
+echo -e "\e[31mCopy Mongodb Repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-echo -e "\e[32mInstalling mongodb\e[0m"
+echo -e "\e[31mInstalling mongodb\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
-set -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongodb.conf
+echo -e "\e[31mUpdating Mongodb listing address\e[0m"
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongodb.conf
 
-echo -e "\e[32mstarting mongodb service\e[0m"
+echo -e "\e[31mstarting mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
