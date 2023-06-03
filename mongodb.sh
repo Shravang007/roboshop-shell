7e8a53f583ef840a0d8c79e42e3f351385799307
Shravang007 <shravangoudgajula@gmail.com>
2023-06-03 20:41:52 +0530

    Start Mongodb service

--- mongodb.sh
+++ mongodb.sh
@@ -8,7 +8,6 @@ yum install mongodb-org -y &>>/tmp/roboshop.log
 echo -e "\e[33mUpdating Mongodb Listen address\e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-echo -e "\e[33mstarting mongodb service\e[0m"
-systemctl enable mongod  &>>/tmp/roboshop.log
+echo -e "\e[33m Starting mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
