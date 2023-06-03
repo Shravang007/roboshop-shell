36d6c68a5c1b45d989a408cb5633725d0aebbe66
Shravang007 <shravangoudgajula@gmail.com>
2023-06-03 20:47:50 +0530

    Start Mongodb service

--- catalogue.sh
+++ catalogue.sh
@@ -23,7 +23,7 @@ echo -e "\e[33mInstall NodeJs Dependencies\e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33mSetup System Service\e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e "\e[33mStart Catalogue Service\e[0m"
 systemctl daemon-reload
@@ -31,7 +31,7 @@ systemctl enable catalogue
 systemctl restart catalogue
 
 echo -e "\e[33mCopy Mongodb Repo File\e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall Mongodb client\e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
