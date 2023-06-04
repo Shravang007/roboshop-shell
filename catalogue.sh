7fb3e98603fe61409b311195f30b7274966765ac
Shravang007 <shravangoudgajula@gmail.com>
2023-06-04 23:23:21 +0530

    schema setup

--- catalogue.sh
+++ catalogue.sh
@@ -3,13 +3,5 @@ source common.sh
 component=catalogue
 
 nodejs
+mongo_schema_setup
 
-
-echo -e "${color}Copy Mongodb Repo File${nocolor}"
-cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
-
-echo -e "${color}Install Mongodb client${nocolor}"
-yum install mongodb-org-shell -y &>>$log_file
-
-echo -e "${color}Load Schema${nocolor}"
-mongo --host mongodb-dev.devopspractice73.online <${app_path}/schema/$component.js &>>$log_file
