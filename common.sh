eeb9d28a5448fca375f0429ec6e68978b3f48a7a
Shravang007 <shravangoudgajula@gmail.com>
2023-06-04 23:22:43 +0530

    schema setup

--- common.sh
+++ common.sh
@@ -41,3 +41,16 @@ nodejs(){
 
 }
 
+mongo_schema-setup() {
+
+  echo -e "${color}Copy Mongodb Repo File${nocolor}"
+  cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+  echo -e "${color}Install Mongodb Service${nocolor}"
+  yum install mongodb-org-shell -y &>>${log_file}
+
+  echo -e "${color}Load Schema${nocolor}"
+  mongo --host mongodb-dev.devopspractice73.online <${app_path}/schema/${component}.js &>>${log_file}
+
+
+}
