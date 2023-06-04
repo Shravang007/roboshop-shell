1b9766d00fb197d352df9844894a4682aaea20d6
Shravang007 <shravangoudgajula@gmail.com>
2023-06-04 22:03:57 +0530

    start mongodb server

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,17 @@
+source common.sh
 
-echo -e "\e[33mCopy Mongodb Repo file\e[0m"
-cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+echo -e " ${color} Copy MongoDB Repo file  ${nocolor} "
+cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo   &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstalling mongodb\e[0m"
-yum install mongodb-org -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mUpdating Mongodb Listen address\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo -e " ${color} Installing MongoDB Server ${nocolor} "
+yum install mongodb-org -y  &>>/tmp/roboshop.log
 
-echo -e "\e[33m Starting mongodb service\e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log
-systemctl restart mongod &>>/tmp/roboshop.log
+
+echo -e " ${color} Update MongoDB Listen Address ${nocolor} "
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+
+echo -e " ${color} Start MongoDB Service ${nocolor} "
+systemctl enable mongod  &>>/tmp/roboshop.log
+systemctl restart mongod  &>>/tmp/roboshop.log
