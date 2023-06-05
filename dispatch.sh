a0a3a5d16a56e5a95b93def6e2d16d0eb07d4bcf
Shravang007 <shravangoudgajula@gmail.com>
2023-06-05 11:14:11 +0530

    setup common.sh

--- dispatch.sh
+++ dispatch.sh
@@ -13,8 +13,7 @@ go mod init ${component} &>>${log_file}
 go get &>>${log_file}
 go build &>>${log_file}
 
-echo -e "${color} Setup SystemD ${component} Service${nocolor}"
-cp /root/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+ systemd_setup
 
 echo -e "${color} Start ${component} Service${nocolor}"
 systemctl daemon-reload &>>${log_file}
