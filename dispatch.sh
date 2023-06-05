413aab94fd585191918e4e399b9df00023e2ec9c
Shravang007 <shravangoudgajula@gmail.com>
2023-06-05 11:30:18 +0530

    dispatch setup

--- dispatch.sh
+++ dispatch.sh
@@ -14,8 +14,3 @@ go get &>>${log_file}
 go build &>>${log_file}
 
  systemd_setup
-
-echo -e "${color} Start ${component} Service${nocolor}"
-systemctl daemon-reload &>>${log_file}
-systemctl enable ${component} &>>${log_file}
-systemctl start ${component} &>>${log_file}
