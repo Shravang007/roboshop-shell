d8745b666185fd5bf752a2729bae57fb06ddb421
Shravang007 <shravangoudgajula@gmail.com>
2023-06-05 13:07:34 +0530

    Start redis service

--- redis.sh
+++ redis.sh
@@ -3,19 +3,22 @@ source common.sh
 
 echo -e " ${color}  Install Redis Repos  ${nocolor} "
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y  &>>${log_path}
-
+stat_check $?
 
 echo -e " ${color}  Enable Redis 6 Version  ${nocolor} "
 yum module enable redis:remi-6.2 -y &>>${log_path}
+stat_check $?
+
 
 echo -e " ${color}  Install Redis  ${nocolor} "
 yum install redis -y &>>${log_path}
-
+stat_check $?
 
 echo -e " ${color}  Update Redis Listen address  ${nocolor} "
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_path}
-
+stat_check $?
 
 echo -e " ${color}  Start Redis Service  ${nocolor} "
 systemctl enable redis  &>>${log_path}
 systemctl restart redis  &>>${log_path}
+stat_check $?
