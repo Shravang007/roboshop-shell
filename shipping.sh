4947a58dab6d21b963e65641e6c6d7bdfdf08310
Shravang007 <shravangoudgajula@gmail.com>
2023-06-05 07:20:28 +0530

    maven setup

--- shipping.sh
+++ shipping.sh
@@ -4,8 +4,8 @@ component=shipping
 maven
 
 
-  echo -e "${color} Install Mysql Client${nocolor}"
-  yum install mysql -y &>>${log_file}
+echo -e "${color} Install Mysql Client${nocolor}"
+yum install mysql -y &>>${log_file}
 
-  echo -e "${color} Loading Schema${nocolor}"
-  mysql -h mysql-dev.devopspractice73.online -uroot -pRoboShop@1 <${app_path}/schema/${component}.sql &>>${log_file}
+echo -e "${color} Loading Schema${nocolor}"
+mysql -h mysql-dev.devopspractice73.online -uroot -pRoboShop@1 <${app_path}/schema/${component}.sql &>>${log_file}
