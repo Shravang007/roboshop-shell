cd2c9a7ccade2d76e7a5ee307241c63996aab98c
Shravang007 <shravangoudgajula@gmail.com>
2023-06-05 10:35:31 +0530

    dispatch setup

--- dispatch.sh
+++ dispatch.sh
@@ -12,23 +12,23 @@ echo -e "${color} Create Application Directory${nocolor}"
 rm -rf ${app_path} &>>${log_file}
 mkdir ${app_path} &>>${log_file}
 
-echo -e "${color} Download Dispatch Content${nocolor}"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>${log_file}
+echo -e "${color} Download ${component} Content${nocolor}"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
 
-echo -e "${color} Extract Dispatch Content${nocolor}"
+echo -e "${color} Extract ${component} Content${nocolor}"
 cd ${app_path} &>>${log_file}
-unzip /tmp/dispatch.zip &>>${log_file}
+unzip /tmp/${component}.zip &>>${log_file}
 
 echo -e "${color} Start Golang${nocolor}"
 cd ${app_path} &>>${log_file}
-go mod init dispatch &>>${log_file}
+go mod init ${component} &>>${log_file}
 go get &>>${log_file}
 go build &>>${log_file}
 
-echo -e "${color} Setup SystemD Dispatch Service${nocolor}"
-cp /root/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service &>>${log_file}
+echo -e "${color} Setup SystemD ${component} Service${nocolor}"
+cp /root/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
-echo -e "${color} Start Dispatch Service${nocolor}"
+echo -e "${color} Start ${component} Service${nocolor}"
 systemctl daemon-reload &>>${log_file}
-systemctl enable dispatch &>>${log_file}
-systemctl start dispatch &>>${log_file}
+systemctl enable ${component} &>>${log_file}
+systemctl start ${component} &>>${log_file}
