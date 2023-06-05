ea85e07db8cf91b77ebcf3fb87c59af5b05491f7
Shravang007 <shravangoudgajula@gmail.com>
2023-06-05 13:11:38 +0530

    Start mysql service

--- mysql.sh
+++ mysql.sh
@@ -4,21 +4,25 @@ component=mysql
 
 echo -e "${color}Disable  ${component} Default Version${nocolor}"
 yum module disable ${component} -y &>>${log_file}
+stat_check $?
+
 
 echo -e "${color}Setup the ${component} repo file${nocolor}"
 cp /root/roboshop-shell/${component}.repo /etc/yum.repos.d/${component}.repo &>>${log_file}
+stat_check $?
 
 echo -e "${color}Install ${component} Community Server${nocolor}"
 yum install ${component}-community-server -y &>>${log_file}
+stat_check $?
 
-echo -e "${color}Start ${component} Sever${nocolor}"
+echo -e "${color}Start ${component} Service${nocolor}"
 systemctl enable ${component}d &>>${log_file}
 systemctl start ${component}d &>>${log_file}
+stat_check $?
 
 echo -e "${color}Setup ${component} Password${nocolor}"
-${component}_secure_installation --set-root-pass RoboShop@1 &>>${log_file}
+${component}_secure_installation --set-root-pass $1 &>>${log_file}
+stat_check $?
 
-echo -e "${color}Check ${component} Password Working${nocolor}"
-${component} -uroot -pRoboShop@ &>>${log_file}
 
 
